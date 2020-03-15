b32a41eee4c43e3e0552ecac1493f53a96e5445c
jcchen0306 <2574533980@qq.com>
2020-03-15 18:06:39 +0800

    添加了c++的继承和多态的使用规则

---
+++ computer_basic/inheritance _polymorphism.cpp
@@ -0,0 +1,127 @@
+//c++继承和多态   inheritance  polymorphism
+//继承的主要目的是冗余的（相同或者类似）代码只保留一份，方便更改和重复利用
+//多态的目的是，用一个父类的指针，指向不同的子类对象，用相同的函数接口名，就可以产生不同的行为
+
+class animalClass
+{
+public:
+    int gender;
+    int weight;
+    int age;
+    char* name;
+
+    animalClass();
+    ~animalClass();
+    void get_weight();
+    virtual void makeSound();      //是虚函数而不是纯虚函数，就要该类的实现也要有
+    virtual void eat()=0;          //纯虚函数，父类不实现该功能，但是要留一个接口给派生类，派生类在实例化时，
+                                   //必须有该函数的该派生类的实现。供子类对象的地址给父类指针时，实现多态
+};
+
+animalClass::animalClass()       //基类的构造函数
+{
+    name = (char*) malloc(20*sizeof(char));
+    weight=0;
+}
+
+animalClass::~animalClass()       //基类的析构函数
+{
+    free(name);
+}
+
+void animalClass::get_weight()       //普通基类函数，所有的子类都可以使用。实现多个类代码共享，避免重复开发和改动时的多处改动。
+{
+    printf("wight is %d",weight);
+}
+
+void animalClass::makeSound()      //虚函数的实现，为多态行为做准备。多态就是父类指针，指向父类对象，就调用父类函数，指向不同的子类，就调用不同的子类的函数。
+{
+    printf("hehhehe");
+}
+
+
+//派生类1
+class dog : public animalClass
+{
+public:
+    char* originPlace;
+    void makeSound();                //子类中不需要重新实现的来自父类的函数，就不要声明，父类中已经实现的类可以直接调用。父类中该函数加了virtual
+                                     //则派生类中该函数自带virtual，所有可加virtual关键字，提醒下一级子类，也可以不加。见谭浩强《C++程序设计(第二版)》p.401-402
+    void eat();                      //父类中声明的纯虚函数，子类中必须要声明且实现。
+    dog();
+    ~dog();
+};
+
+void dog::makeSound()               //在类定义外实现虚函数，不需要加virtual；
+{
+
+    printf("wang wang wang \n");
+}
+
+void dog::eat()
+{
+    printf("eat bones\n");
+}
+
+dog::dog()
+{
+    originPlace = (char*) malloc(20*sizeof(char));
+}
+
+dog::~dog()
+{
+    free(originPlace);
+}
+
+
+//派生类2
+class cat : public animalClass
+{
+public:
+    char* color;
+    void makeSound();                //子类中不需要重新实现的来自父类的函数，就不要声明，父类中已经实现的类可以直接调用。父类中该函数加了virtual
+                                     //则派生类中该函数自带virtual，所有可加virtual关键字，提醒下一级子类，也可以不加。见谭浩强《C++程序设计(第二版)》p.401-402
+    void eat();                      //父类中声明的纯虚函数，子类中必须要声明且实现。
+    cat();
+    ~cat();
+};
+
+void cat::makeSound()               //在类定义外实现虚函数，不需要加virtual；
+{
+
+    printf("miao miao miao \n");
+}
+
+void cat::eat()
+{
+    printf("eat fish\n");
+}
+
+cat::cat()
+{
+    color = (char*) malloc(20*sizeof(char));
+}
+
+cat::~cat()
+{
+    free(color);
+}
+
+
+//以下是测试函数
+#include"list.h"
+
+int main()
+{
+    animalClass* group[2];
+    group[0] = new cat();
+    group[1] = new dog();
+
+    for(int i=0;i<2;i++)
+    {
+        group[i]->eat();
+        group[i]->makeSound();
+    }
+}
+
+
